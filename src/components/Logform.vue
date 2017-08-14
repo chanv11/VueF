a8b044a29f6007320cd4d506e5e2cd70aa3a2f21
chanv11 <m15988174410_2@163.com>
2017-08-14 18:44:40 +0800

    Hap

---
+++ src/components/Logform.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <div class="log-form">
+        <div class="g-form-line">
+            <span class="g-form-label">用户名</span>
+            <div class="g-form-input">
+                <input type="text" placeholder="请输入用户名">
+            </div>
+            <div class="g-form-error" >error1</div>
+        </div>       
+        <div class="g-form-line">
+            <span class="g-form-label">密码</span>
+            <div class="g-form-input">
+                <input type="password" placeholder="请输入密码">
+            </div>
+            <div class="g-form-error">error2</div>
+        </div>    
+        <div class="g-for-line">
+            <div class="g-form-btn">
+                <a class="btn">登陆</a>
+            </div>
+            </div>
+            <p>error3</p>     
+    </div>
+</div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+
+</style>
